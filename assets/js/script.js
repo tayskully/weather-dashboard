5cdc3d33954b753abc30eba2308f869b89b8b132
Taylor <taysully94@gmail.com>
2023-09-01 16:34:17 -0400

    first commit, added html structure, linked css, bootstrap, JQuery, added API key

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+//weather API key 
+var APIkey = "91d73ec1749355e3bf23af5b11eb2ac6";
+var city = "orlando"; //"user input";
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
+
+//api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
