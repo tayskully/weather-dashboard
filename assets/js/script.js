17d771e1b9fa63012517c38abeb9664a924e9ca8
Taylor <taysully94@gmail.com>
2023-09-02 19:54:48 -0400

    adds styling to give general layout and colors, adds basic html structure, the text inputs and buttons, and the card that will display the weather data, adds the query selectors and api key to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
 //weather API key 
-var APIkey = "91d73ec1749355e3bf23af5b11eb2ac6";
-var city = "orlando"; //"user input";
+var APIKey = "91d73ec1749355e3bf23af5b11eb2ac6";
+var cityInputEl = document.querySelector('#city-search')
+var submitBtn = document.querySelector('#submit-btn')
 var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
 
-//api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+// api.openweathermap.org/data/2.5/forecast?q=Orlando&appid=91d73ec1749355e3bf23af5b11eb2ac6
+
